b64c5b1de703117184e9f597735ea39633196b4b
ARPIT KUMAWAT <arpitjalwal18@gmail.com>
2026-06-12 02:27:29 +0530

    Delete db_connect.py

--- db_connect.py
+++
@@ -1,5 +0,0 @@
-import sqlite3
-def get_db_connection():
-    # check_same_thread=False zaroori hai Streamlit ke liye 
-    conn = sqlite3.connect("project_data.db", check_same_thread=False)
-    return conn
